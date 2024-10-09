652bd2d11e35ee107c1b8e822148bcf4ae8f9d9b
noahvaks <60456332+noahvaks@users.noreply.github.com>
2024-10-09 14:49:20 -0700

    Update 206.py

--- 206.py
+++ 206.py
@@ -5,6 +5,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        curr = head
+        prev = None
+        
+        while curr:
+            temp = curr.next #the actual next one
+            curr.next = prev #change to now point backwards
+            prev = curr # move forward
+            curr = temp # move forward
+            
+        return prev
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
 # can just traverse the list and add the current node to the head of a new list
 class Solution:
